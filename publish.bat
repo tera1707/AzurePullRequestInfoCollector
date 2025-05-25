4c74349b343f0539d94527f95fc4669dcc1b7c2b
tera1707 <Masa1017>
2025-05-25 23:18:46 +0900

    dotnet publishしたexeを動かすとアプリクラッシュするのを修正

--- publish.bat
+++
@@ -1,3 +0,0 @@
-cd %~dp0
-dotnet publish PullReqInfoCollector.sln -c Debug -r win-x64 --output .\publish --self-contained -p:RuntimeIdentifier=win-x64 -p:Platform=x64 -p:PublishSingleFile=true
-pause
